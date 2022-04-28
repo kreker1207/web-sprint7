63b954012102c2ddf11d52a350512207d3c1120e
Ivan Barantskiy <93523707+kreker1207@users.noreply.github.com>
2022-04-28 23:20:30 +0300

    Add files via upload

---
+++ t03_files/FileList.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+module.exports = class FileList {
+    getList() {
+        let dir = './tmp'
+        let files = []
+        fs.readdirSync(dir).forEach(file => {
+            let data = fs.readFileSync('tmp/' + file, 'utf8')
+            files.push({name: file, content: data})
+        })
+        return files
+    }
+    hasFiles() {
+        return fs.existsSync('./tmp')
+    }
+    getHTMLList() {
+        return this.getList()
+    }
+}
